9a8bc7dc3981911bce6d29df23c483cf99453300
Lyydia Lehtinen <lehtinen.lyydia@gmail.com>
2024-12-06 22:52:24 +0200

    Finish drone movementa.

--- drone.py
+++ drone.py
@@ -6,8 +6,8 @@ directions = [pygame.Vector2(0,1), pygame.Vector2(1,0), pygame.Vector2(0,-1), py
 class Drone(object.GameObject):
     def __init__(self, speed, height):
         super().__init__()
-        self.location = pygame.Vector2(-100, 0)
-        self.direction = 0
+        self.location = pygame.Vector2(0, 1)
+        self.direction = 2
         self.speed = 50 #m/s
         self.velocity = directions[self.direction] * self.speed * settings.PIXELS_PER_METER * settings.TICK_SPEED
         self.height = height
@@ -18,14 +18,13 @@ class Drone(object.GameObject):
     def fixed_tick(self, dt):
         #collision
         #moving
-        print(self.velocity)
-        if self.location[1] < -settings.SCREEN_HEIGHT/2 or self.location[1] > settings.SCREEN_HEIGHT/2:
-            self.location -= 2*directions[self.direction]
+        if self.direction % 2 == 0 and self.location[1] < 0 or self.location[1] > settings.SCREEN_HEIGHT:
+            self.location -= self.velocity
             self.direction += 1
             self.direction %= 4
             self.velocity = directions[self.direction] * self.speed * settings.PIXELS_PER_METER * settings.TICK_SPEED
             self.sprite = object.Sprite(pygame.Vector2(self.sight, 1), (255, 0, 255, 50), self.location)
-        elif self.location[0] % self.sight < 1 and self.location[0] % self.sight > -1:
+        elif self.direction % 2 == 1 and self.location[0] % self.sight < 1 or self.location[0] % self.sight > self.sight-1:
             self.location += 2*directions[self.direction]
             self.direction += 1
             self.direction %= 4
