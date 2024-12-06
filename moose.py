e65e74d9d7050fa3b7e687f3e89f2b81f787be42
Lyydia Lehtinen <lehtinen.lyydia@gmail.com>
2024-12-06 18:51:20 +0200

    Add colliding bool.

--- moose.py
+++ moose.py
@@ -8,6 +8,7 @@ class Moose(object.GameObject):# Korkeus, pituus, reviiri
         self.height = height
         self.length = length
         self.territory = territory
+        self.colliding = False
         self.sprite = object.Sprite(pygame.Vector2(self.length*2, self.length), (255, 100, 0), self.location)
 
     def fixed_tick(self, dt):
