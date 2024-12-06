14c3b30d3c058486849eb9ad9dec17e7e794f197
Lyydia Lehtinen <lehtinen.lyydia@gmail.com>
2024-12-06 22:21:44 +0200

    Set camera to corner.

--- camera.py
+++ camera.py
@@ -4,7 +4,7 @@ import settings, object
 class Camera(object.GameObject):
     def __init__(self):
         super().__init__()
-        self.location = pygame.Vector2(0, 0)
+        self.location = pygame.Vector2(settings.SCREEN_WIDTH/2, settings.SCREEN_HEIGHT/2)
 
     def fixed_tick(self, dt):
-        self.location = self.location.lerp(settings.world_to_screen((0,0)), 0.02*dt)
+        self.location = self.location.lerp(self.location, 0.02*dt)
